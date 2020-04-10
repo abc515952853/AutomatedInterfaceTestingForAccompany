4001fa014ff0a99d8129e85d6cb38adcfb4c5fa8
abc515952853 <515952853@qq.com>
2020-04-10 09:48:33 +0800

    commit on master

--- tools/Smtp.py
+++ tools/Smtp.py
@@ -6,36 +6,39 @@ import email.mime.text
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-# import ReadConfig 
-
-# readconfig=ReadConfig.ReadConfig()
+from tools import ReadConfig
 
 class Smtp:
     def __init__(self,):
+        self.configdata = ReadConfig.ReadConfig()
         self.msg = email.mime.multipart.MIMEMultipart()
-        self.msg['from'] = '515952853@qq.com'#readconfig.get_basedata('email_sendaddr')
-        self.msg['to'] = '515952853@qq.com'##readconfig.get_basedata('email_recipientaddrs')
-        self.msg['subject'] ='TestResult'## readconfig.get_basedata('email_subject')
-        content ='TestResultFor陪伴APP接口'## readconfig.get_basedata('email_content')
+        self.msg['from'] = self.configdata.get_data('EAMAIL','email_sendaddr')
+        self.msg['to'] = self.configdata.get_data('EAMAIL','email_recipientaddrs')
+        self.msg['subject'] = self.configdata.get_data('EAMAIL','email_subject')
+        content = self.configdata.get_data('EAMAIL','email_content')
         txt = email.mime.text.MIMEText(content, 'plain', 'utf-8')
         self.msg.attach(txt)
         
 
     def add_accessory(self,accessoryfile):
+        print(accessoryfile)
         part = MIMEApplication(open(accessoryfile,'rb').read())
-        part.add_header('Content-Disposition', 'attachment', filename=accessoryfile)
+        part.add_header('Content-Disposition', 'attachment', filename=accessoryfile.split('\\')[-1])#参数为路径，文件名截取路径\后字段
         self.msg.attach(part)
 
     def send_email(self,):
-        smtpHost = 'smtp.qq.com'##readconfig.get_basedata('email_smtphost')
-        password = 'lhenyaejzrlzbgcb'#readconfig.get_basedata('email_password')
-        print(smtpHost)
+        smtpHost = self.configdata.get_data('EAMAIL','email_smtphost')
+        password = self.configdata.get_data('EAMAIL','email_password')
         smtp = smtplib.SMTP()
         smtp.connect(smtpHost, '25')
         smtp.login(self.msg['from'], password)
-        smtp.sendmail(self.msg['from'], self.msg['to'], str(self.msg))
-        print("邮件发送成功！")
-        smtp.quit()
+        try:
+            smtp.sendmail(self.msg['from'], self.msg['to'], str(self.msg))
+            print("邮件发送成功！")
+        except Exception as ex_results:
+            print("抓了一个异常：",ex_results)
+        finally:
+            smtp.quit()
 
 
         
